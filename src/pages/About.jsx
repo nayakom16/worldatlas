9c54d3e29e9967962aedf0a4b202c79ccac6ac11
nayakom16 <nayakankush71@gmail.com>
2025-05-31 21:07:52 +0530

    worldAtlas

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,21 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <main className='hero-section main'>
+        <div className="container grid grid-two-cols">
+          <div className="hero-content">
+            <h1 className="heading-xl">
+              About Us
+            </h1>
+            <p className="paragraph">
+              We are dedicated to exploring the world, one country at a time. Our mission is to provide insightful information and engaging content about different cultures, places, and experiences.
+            </p>
+          </div>
+          <div className="hero-image">
+            <img src="/about.png" alt="About Us" className='banner-image' />
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
